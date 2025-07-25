4ed11468e698248d5c8725633d8887a7331311e4
Robert Solis-Stevenson <98938524+RMSS86@users.noreply.github.com>
2025-07-24 19:27:27 -0600

    Readme.md updated

--- backend/utils/regs/FireBaseAccess.js
+++ backend/utils/regs/FireBaseAccess.js
@@ -1,9 +1,12 @@
-export default _firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,//; "AIzaSyBoLHuHxbxUonYqso9sg2o_f_EqqLRjfVw",
-  authDomain:process.env.FIREBASE_AUTHDOMAIN, // "trax-408c4.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID, //"trax-408c4",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, //"trax-408c4.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MSGSEND_ID,// "1020932068120",
-  appId: process.env.FIREBASE_APP_ID, // "1:1020932068120:web:a0759c90f5f9ad1aa3046d",
-  measurementId: process.env.FIREBASE_MESUREMENT_ID, // "G-VYZ034WHY1",
+const _firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY, //;
+  authDomain: process.env.FIREBASE_AUTHDOMAIN, //
+  projectId: process.env.FIREBASE_PROJECT_ID, //
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, //
+  messagingSenderId: process.env.FIREBASE_MSGSEND_ID, // 
+  appId: process.env.FIREBASE_APP_ID, // "",
+  measurementId: process.env.FIREBASE_MESUREMENT_ID, //
 };
+
+
+export  {_firebaseConfig};
